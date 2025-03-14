b6b751dee2da977b7eabe808dcbd851693baed4a
Mariella Gustafsson <mariella_gustafsson@hotmail.se>
2025-03-13 17:08:06 -0700

    - Lägg till footeraccordion - Lägg till footer-ikonlista

---
+++ client/src/component/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import FooterAccordion from "./FooterAccordion";
+import FooterIcons from "./FooterIcons";
+
+function Footer () {
+  return (
+    <>
+    <FooterIcons />
+    <FooterAccordion />
+    </>
+  )
+}
+
+export default Footer;
