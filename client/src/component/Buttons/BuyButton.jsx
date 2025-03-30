d1718eb014d3889928c3630c6e9fdaa6ae67dd56
Mariella Gustafsson <mariella_gustafsson@hotmail.se>
2025-03-30 18:21:01 +0200

    - Lägg till varukorgstabell mobilversion - Lägg till Checkout

---
+++ client/src/component/Buttons/BuyButton.jsx
@@ -0,0 +1,17 @@
+function ToCart () {
+
+  return (
+    <div
+      className="flex justify-center"
+    >
+      <button
+        type="button"
+        className="border-1 my-4 border-black p-4 bg-[#5D2B7E] text-[#e9acca] font-bold shadow-md shadow-black w-[40%]"
+      >
+        Köp
+      </button>
+    </div>
+  )
+};
+
+export default ToCart;
--- client/src/component/CartCheckout/CheckoutForm.jsx
+++ client/src/component/CartCheckout/CheckoutForm.jsx
@@ -1,38 +1,38 @@
 function CheckoutForm () {
   return (
     <>
-    <h2 className="text-2xl font-bold flex flex-col items-center">
+      <h2 className="text-2xl font-bold flex flex-col items-center">
         Kunduppgifter
       </h2>
-      
+        
       <form className="p-4">
         <div className="flex flex-col sm:grid sm:grid-cols-2 sm:gap-4 lg:grid-cols-3">
           <div className="flex flex-col">
             <div className="flex flex-col gap-2 mb-2">
               <label for="firstName">Förnamn:</label>
-              <input type="text" id="firstName" name="firstName" />
+              <input type="text" id="firstName" name="firstName" className="border-1 border-black" />
             </div>
             <div className="flex flex-col gap-2 mb-2">
               <label for="lastName">Efternamn:</label>
-              <input type="text" id="lastName" name="lastName" />
+              <input type="text" id="lastName" name="lastName" className="border-1 border-black" />
             </div>
           </div>
           <div className="flex flex-col gap-2 mb-4">
             <label for="email">E-post:</label>
-            <input type="email" id="email" name="email" />
+            <input type="email" id="email" name="email" className="border-1 border-black" />
           </div>
         </div>
-        <fieldset className="flex flex-col gap-2 mb-4 border-black border-2 p-3 lg:grid lg:grid-cols-2">
+        <fieldset className="flex flex-col gap-2 mb-4 border-black border-2 p-5 lg:grid lg:grid-cols-2 lg:w-[90%]">
           <legend className="text-2xl">Adress</legend>
           <div className="flex flex-col p-3">
             <label for="street">Gata</label>
-            <input type="text" id="street" name="street" />
+            <input type="text" id="street" name="street" className="border-1 border-black"/>
 
             <label for="postNumber">Postnummer</label>
-            <input type="text" id="postNumber" name="postNumber" pattern="[0-9]{5}" class="w-[20%]" />
+            <input type="text" id="postNumber" name="postNumber" pattern="[0-9]{5}" className="border-1 border-black w-[20%]"/>
 
             <label for="city">Stad</label>
-            <input type="text" id="city" name="city" class="w-[70%]" />
+            <input type="text" id="city" name="city" className="border-1 border-black w-[70%]" />
           </div>
         </fieldset>
         <div className="mb-4">
@@ -45,12 +45,9 @@ function CheckoutForm () {
                 Jag vill ta emot nyhetsbrev
           </label>
         </div>
-        <div className="flex justify-center">
-          <div class="button-styles flex justify-center items-center w-1/2">
-            <button type="submit">Köp</button> 
-          </div>
-        </div>
       </form>
-      </>
+    </>
   )
 }
+
+export default CheckoutForm;
