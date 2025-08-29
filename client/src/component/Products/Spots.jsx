85a030910cfaa644f7be6f0327448ee1919a3535
Mariella Gustafsson <mariella_gustafsson@hotmail.se>
2025-08-29 23:06:33 +0200

    - Göra spot dynamiska

--- client/src/component/Products/Spots.jsx
+++ client/src/component/Products/Spots.jsx
@@ -1,87 +1,58 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { fetchCategories } from "../../services/api";
+import { useState } from "react";
 
 function Spots () {
 
+  const [spots, setSpots] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchCategories()
+    .then(data => {
+      const categories = data;
+      const randomSpots = categories.sort(() => 0.5 - Math.random());
+      setSpots(randomSpots.slice(0, 3));
+    })
+    .catch(err => {
+      setError(err);
+      setLoading(false);
+      console.log("Produkterna kunde inte laddas. Felmeddelande: " + err)
+    })}, []);
+
   return (
     <section className="flex flex-row gap-4 m-4">
-      <Link 
-        to="/"
-        className="w-1/3 relative opacity-90">
-        <img
-          src="http://localhost:8000/images/SpotElegant.jpg"
-          alt="Modell klädd i elegant stil"
-        />
-        <h2 className={`
-          absolute
-          text-center
-          text-6xl
-          font-bold
-          text-[#edd1ff]
-          top-[50%]
-          left-[50%]
-          -translate-x-1/2
-          -translate-y-1/2
-          transform
-          text-shadow
-          uppercase
-          font-[Rowdies]
-        `}>
-          Elegant
-        </h2>
-      </Link>
-      <Link 
-        to="/"
-        className="w-1/3 relative">
-        <img
-          className="opacity-90"
-          src="http://localhost:8000/images/SpotCasual.jpg"
-          alt="Modell klädd i casual stil"
-        />
-        <h2 className={`
-          absolute
-          text-center
-          text-6xl
-          font-bold
-          text-[#edd1ff]
-          top-[50%]
-          left-[50%]
-          -translate-x-1/2
-          -translate-y-1/2
-          transform
-          text-shadow
-          uppercase
-          font-[Rowdies]
-        `}>
-          Casual
-        </h2>
-      </Link>
-      <Link 
-        to="/"
-        className="w-1/3 relative">
-        <img
-          className="opacity-90"
-          src="http://localhost:8000/images/SpotSporty.jpg"
-          alt="Modell klädd  i sport stil"
-        />
-        <h2 className={`
-          absolute
-          text-center
-          text-6xl
-          font-bold
-          text-[#edd1ff]
-          top-[50%]
-          left-[50%]
-          -translate-x-1/2
-          -translate-y-1/2
-          transform
-          text-shadow
-          uppercase
-          font-[Rowdies]
-        `}>
-          Sport
-        </h2>
-      </Link>
-  </section>
+      {
+        spots.map((spot => (
+          <Link 
+            to={`/categories/${spot.category_name}`}
+            className="w-1/3 relative opacity-90">
+            <img
+              src={`http://localhost:8000/${spot.category_picture}`}
+              alt="Modell klädd i elegant stil"
+            />
+            <h2 className={`
+              absolute
+              text-center
+              text-6xl
+              font-bold
+             text-[#edd1ff]
+              top-[50%]
+              left-[50%]
+              -translate-x-1/2
+              -translate-y-1/2
+              transform
+              text-shadow
+              uppercase
+              font-[Rowdies]
+              `}>
+              {spot.category_name}
+            </h2>
+          </Link>
+        )))
+      }
+    </section>
   )
 };
 
